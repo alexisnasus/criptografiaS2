09f51caa26aea2a398a739cc11794ba496d86e9c
alexisnasus <lexislema10@gmail.com>
2024-11-05 23:44:05 -0300

    lab4 update xd

---
+++ Lab4/cript.py
@@ -0,0 +1,7 @@
+from Crypto.Cipher import AES
+from Crypto.Hash import HMAC, SHA256
+from Crypto.Random import get_random_bytes
+
+txt = input("ingresa texto a cifrarxd: ")
+iv = input("ingresa vector de inicialización: ")
+txt = input("ingresa key xd: ")
